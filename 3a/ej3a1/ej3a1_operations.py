94b372b1337b73ee434898a4e18fb591b0b4a788
Carlos Yáñez <carlosyanez997@gmail.com>
2025-12-02 01:50:01 +0100

    Add solution for exercise 3 a1

--- 3a/ej3a1/ej3a1_operations.py
+++ 3a/ej3a1/ej3a1_operations.py
@@ -2,20 +2,16 @@
 
 
 def add(num_1: float, num_2: float) -> float:
-    # Write here your code
-    pass
+    return num_1 + num_2
 
 
 def subtract(num_1: float, num_2: float) -> float:
-    # Write here your code
-    pass
+    return num_1 - num_2
 
 
 def multiply(num_1: float, num_2: float) -> float:
-    # Write here your code
-    pass
+    return num_1 * num_2
 
 
 def divide(num_1: float, num_2: float) -> float:
-    # Write here your code
-    pass
+    return num_1 / num_2
