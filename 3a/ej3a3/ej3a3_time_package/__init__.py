77b41601607763755db7cb080af73840c74055dd
Carlos Yáñez <carlosyanez997@gmail.com>
2025-12-02 01:55:17 +0100

    Add solution for exercise 3 a3

--- 3a/ej3a3/ej3a3_time_package/__init__.py
+++ 3a/ej3a3/ej3a3_time_package/__init__.py
@@ -1,2 +1,5 @@
 # write import for convert_date_format
 # write import for date_operations
+from . import convert_date_format
+from . import date_operations
+__all__ = ["convert_date_format", "date_operations"]
